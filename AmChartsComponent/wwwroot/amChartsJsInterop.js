5af0024d943fb8ac69cb1fdc993f0b77329ed338
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2020-12-23 13:26:01 +0300

    add slider

--- AmChartsComponent/wwwroot/amChartsJsInterop.js
+++ AmChartsComponent/wwwroot/amChartsJsInterop.js
@@ -10,7 +10,8 @@ window.amcharts = {
     //export function init(countPoints, chartModelsJson) {
     init: function (countPoints, chartModelsJson) {
 
-        charts = [];        
+        charts = [];  
+        slider = {};
         countGPoints = countPoints;
 
         am4core.ready(function () {
@@ -20,18 +21,57 @@ window.amcharts = {
             am4core.useTheme(am4themes_animated);
             // Themes end
 
-            var chartModels = JSON.parse(chartModelsJson);
-            var isNeedScroll = true;
+            var chartModels = JSON.parse(chartModelsJson);           
             var number = 1;
+
+            slider = createSlider();
+
             chartModels.forEach((element) => {
 
                 var sensorName = "Sensor " + number.toString();
-                charts.push(createChart(element.Id, sensorName, isNeedScroll));
-                isNeedScroll = false;
+                charts.push(createChart(element.Id, sensorName));                
                 number++;
             })
 
-            function createChart(idChart, header, isNeedScroll) {
+            function createSlider() {
+
+                var chart = am4core.create("sliderdiv", am4charts.XYChart);
+                chart.data = generateChartData();
+                // Create axes
+                var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+                dateAxis.dataFields.category = "Date";
+                dateAxis.renderer.grid.template.location = 0.5;
+                dateAxis.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+                dateAxis.renderer.minGridDistance = 50;
+
+                dateAxis.events.on("startendchanged", dateAxisChanged);                
+
+                var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+                // Create series
+                var series = chart.series.push(new am4charts.LineSeries());
+                series.dataFields.valueY = "value";
+                series.dataFields.dateX = "date";
+                series.strokeWidth = 2
+                series.strokeOpacity = 0.3;
+
+                //slider
+                chart.scrollbarX = new am4core.Scrollbar();
+
+                chart.plotContainer.visible = false;
+                chart.leftAxesContainer.visible = false;
+                chart.rightAxesContainer.visible = false;
+                chart.bottomAxesContainer.visible = false;
+
+                chart.cursor = new am4charts.XYCursor();
+                chart.cursor.xAxis = dateAxis;
+
+                chart.padding(0, 15, 0, 15);
+
+                return chart;
+            }
+
+            function createChart(idChart, header) {
 
                 // Create chart instance
                 var chart = am4core.create(idChart, am4charts.XYChart);
@@ -51,15 +91,7 @@ window.amcharts = {
                 dateAxis.groupCount = 1000;
                 //подписка на изменение масштаба и скрол графика
                 dateAxis.events.on("startendchanged", dateAxisChanged);
-                function dateAxisChanged(ev) {
-
-                    charts.forEach((element) => {
-
-                        element.cursor.xAxis.start = ev.target.start;
-                        element.cursor.xAxis.end = ev.target.end;
-                    })
-                }
-
+                
                 var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
                 // Create series
@@ -79,13 +111,6 @@ window.amcharts = {
                 //bullet.circle.stroke = am4core.color("#fff");
                 //bullet.circle.strokeWidth = 1;
 
-                // Add scrollbar
-                if (isNeedScroll) {
-                    chart.scrollbarX = new am4core.Scrollbar();
-                    //chart.scrollbarX = new am4charts.XYChartScrollbar();
-                    //chart.scrollbarX.series.push(series);
-                }
-
                 // Add cursor
                 chart.cursor = new am4charts.XYCursor();
                 chart.cursor.xAxis = dateAxis;
@@ -94,6 +119,17 @@ window.amcharts = {
                 chart.svgContainer.autoResize = false;
 
                 return chart;
+            }
+
+            function dateAxisChanged(ev) {
+                
+                slider.cursor.xAxis.start = ev.target.start;
+                slider.cursor.xAxis.end = ev.target.end;
+                charts.forEach((element) => {
+
+                    element.cursor.xAxis.start = ev.target.start;
+                    element.cursor.xAxis.end = ev.target.end;
+                })
             }
         });
     }
